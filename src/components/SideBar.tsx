6de285c0b487a5544634f8b829b67aaed01ef1c2
bruno0907 <bruno0907@gmail.com>
2021-05-05 00:30:43 -0300

    feat: added SideBar and Content components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from 'react';
+
+import { Button } from '../components/Button';
+
+import '../styles/sidebar.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar({ genres, handleClickButton }: GenreResponseProps) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
